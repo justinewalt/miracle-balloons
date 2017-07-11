3fb33a6215e4098374eb2c3f80c4f2e3bbd8be06
tschnide <travis.schnider@gmail.com>
2017-07-10 19:26:13 -0600

    enabled options dropdown function

--- src/components/Reorder.js
+++ src/components/Reorder.js
@@ -1,53 +1,14 @@
-	import React from 'react';
-	import { Dropdown } from 'semantic-ui-react';
-
-	// 	var numberOfBaloons = [
-	// 	    {
-	// 	    	key: 1000,
-	// 	    	text: 1000,
-	// 	    	value: 1000,
-	// 	    },
-	// 	    {
-	// 	    	key: 2000,
-	// 	    	text: 2000,
-	// 	    	value: 2000
-	// 	    },
-	// 	    {
-	// 	    	key: 3000,
-	// 	    	text: 3000,
-	// 	    	value: 3000,
-	// 	    },
-	// 	    {
-	// 	    	key: 4000,
-	// 	    	text: 4000,
-	// 	    	value: 4000,
-	// 	    },
-	// 	]
-	
-	// 	const Reorder = () => 
-	// 		(
-	// 	  		<Dropdown placeholder='Order quantity' selection options={numberOfBaloons} value/>
-	// 	  	)
-
-	// export default Reorder 	
+import React from 'react';
+// import { Dropdown } from 'semantic-ui-react';
 
 class Reorder extends React.Component {
-	 
-	 // constructor(props) {
-  //    super(props);
-		
-  //    this.state = {
-  //       data: 'numberOfBaloons'
-  //    }
-
-  //     this.updateState = this.updateState.bind(this);
-
-  //    };
-
 	render() {
+		var numberOfBaloons = [ 100, 200, 300]
 		return(
 			<select>
-				<option>test</option>
+				{numberOfBaloons.map(function(num) {
+					return(<option value={num}>{num}</option>);
+				})}
 			</select>
 		)
 	}
