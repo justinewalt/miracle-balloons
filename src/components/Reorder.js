37f55fa8e8995517e84ed1dc3bec68c544c8e339
tschnide <travis.schnider@gmail.com>
2017-07-10 21:24:49 -0600

    added local storage persistence

--- src/components/Reorder.js
+++ src/components/Reorder.js
@@ -1,18 +1,51 @@
 import React from 'react';
 // import { Dropdown } from 'semantic-ui-react';
+var SelectedOrder = function (props) {
+
+	return (
+		<option value={props.num} selected={props.active}>{props.num}</option>
+	)
+}
+
+
 
 class Reorder extends React.Component {
+	
+
+	componentWillMount() {
+		function getDefaultState () {
+			if (window && window.localStorage) {
+				return window.localStorage.balloonCount || '';
+			} 
+			return '';
+		};
+		this.setState({currentDropdownState: getDefaultState()});
+	}
+	
+	selectHandler = (e) => {
+		window.localStorage.setItem('balloonCount', e.target.value)
+	}
+
 	render() {
-		var numberOfBaloons = [ 100, 200, 300]
+		var numberOfBaloons = [ 100, 200, 300, 400]
+		var currentDropdownState = this.state.currentDropdownState;
 		return(
-			<select>
-				{numberOfBaloons.map(function(num) {
-					return(<option value={num}>{num}</option>);
-				})}
-			</select>
+			<select
+			onChange={this.selectHandler}
+		>
+			{numberOfBaloons.map(function(num) {
+				return (<SelectedOrder 
+					num={num}
+					active={num.toString() === currentDropdownState.toString()} 
+				/>)
+			})}
+		</select>
 		)
 	}
 }
 
 export default Reorder;
+
+
+
 
