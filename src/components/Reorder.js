f11691d7e038811f278c25c7ce805d95985377a0
tschnide <travis.schnider@gmail.com>
2017-07-10 19:18:22 -0600

    added reorder component

--- src/components/Reorder.js
+++ src/components/Reorder.js
@@ -1,18 +1,57 @@
-	import React from 'react'
-	import { Dropdown } from 'semantic-ui-react'
+	import React from 'react';
+	import { Dropdown } from 'semantic-ui-react';
 
-		var options = [
-		    {
-		      text: 'Jenny Hess',
-		      value: 'Jenny Hess',
-		    },
-		    {
-		      text: 'ME',
-		      value: 'ME',
-		    }
-		]
+	// 	var numberOfBaloons = [
+	// 	    {
+	// 	    	key: 1000,
+	// 	    	text: 1000,
+	// 	    	value: 1000,
+	// 	    },
+	// 	    {
+	// 	    	key: 2000,
+	// 	    	text: 2000,
+	// 	    	value: 2000
+	// 	    },
+	// 	    {
+	// 	    	key: 3000,
+	// 	    	text: 3000,
+	// 	    	value: 3000,
+	// 	    },
+	// 	    {
+	// 	    	key: 4000,
+	// 	    	text: 4000,
+	// 	    	value: 4000,
+	// 	    },
+	// 	]
+	
+	// 	const Reorder = () => 
+	// 		(
+	// 	  		<Dropdown placeholder='Order quantity' selection options={numberOfBaloons} value/>
+	// 	  	)
 
-const Reorder = () => (
-  <Dropdown placeholder='Select Friend' fluid selection options={options} />
-)
-	export default Reorder 	
+	// export default Reorder 	
+
+class Reorder extends React.Component {
+	 
+	 // constructor(props) {
+  //    super(props);
+		
+  //    this.state = {
+  //       data: 'numberOfBaloons'
+  //    }
+
+  //     this.updateState = this.updateState.bind(this);
+
+  //    };
+
+	render() {
+		return(
+			<select>
+				<option>test</option>
+			</select>
+		)
+	}
+}
+
+export default Reorder;
+
